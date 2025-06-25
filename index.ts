1477378c4d9f2be6c208747f3af1e9f81d829c59
Ron Alex <ronmathewalx@gmail.com>
2025-06-25 16:10:25 -0500

    Adding stuff

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
 const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req: any, res: any) => {
+    const {method, url} = req;
+    
+    res.setHeader('Content-Type', 'application/json');
+
+    if(method === 'GET' && url === "/") {
+        res.writeHead(200);
+        res.end("E - OO - E - A - E - OO - O - A - E - E - OO - E - A - E");
+    }
+    else{
+        res.writeHead(404);
+        res.end("COULD NOT FIND RESOURCE");
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ router.ts
@@ -0,0 +1,54 @@
+type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+
+type RouteHandler = (req: any, res: any) => void;
+
+interface RouteMap {
+    [path: string]: RouteHandler;
+}
+
+interface Routes{
+    [method: string]: RouteMap;
+}
+
+class Router{
+    private routes: Routes;
+    constructor(){
+
+        this.routes = {};
+    }
+
+    addRoute(method: HTTPMethod, path: string, handler: RouteHandler): void{
+        if(!this.routes[method]) {
+            this.routes[method] = {};
+        }
+        this.routes[method][path] = handler;
+    }
+
+    get(path: string, handler: RouteHandler): void{
+        this.addRoute('GET', path, handler);
+    }
+
+    post(path: string, handler: RouteHandler): void{
+        this.addRoute('POST', path, handler);
+    }
+
+    handle(req: any, res: any){
+        const {method, url} = req;
+        const path = url.split('?')[0];
+
+        const methodRoutes = this.routes[method];
+        if(methodRoutes) {
+            const handler = methodRoutes[path];
+            if(handler) {
+                return handler(req, res);
+            }
+        }
+
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({error: "NOT FOUND"}));
+    }
+}
+
+module.exports = {
+    Router: Router
+}
